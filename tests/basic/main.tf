2f1506bb864008382143cf8bc6f139acc8529064
Nikita Kulakov <nvkulakov.dev@gmail.com>
2025-07-18 19:36:47 +0300

    add both tcp and udp

--- tests/basic/main.tf
+++ tests/basic/main.tf
@@ -16,7 +16,7 @@ module "selfhosted" {
 
   vms = {
     ubuntu = {
-      1 = {
+      test-basic-1 = {
         pools = {
           images = "alluvium-test-images"
           disks  = "alluvium-test-disks"
@@ -29,7 +29,7 @@ module "selfhosted" {
         disk_size = 30
         hostfwd   = []
       }
-      2 = {
+      test-basic-2 = {
         pools = {
           images = "alluvium-test-images"
           disks  = "alluvium-test-disks"
@@ -41,6 +41,28 @@ module "selfhosted" {
         memory    = 2048
         disk_size = 30
         hostfwd   = []
+      }
+      test-basic-3 = {
+        pools = {
+          images = "alluvium-test-images"
+          disks  = "alluvium-test-disks"
+        }
+        base = {
+          source = "/home/nikit/vms/images/focal-server-cloudimg-amd64-disk-kvm.img"
+        }
+        vcpu      = 2
+        memory    = 2048
+        disk_size = 30
+        hostfwd = [
+          {
+            from = 19998
+            to   = 19998
+          },
+          {
+            from = 19999
+            to   = 19999
+          }
+        ]
       }
     }
   }
